91da446d037382547533f42dd9737fe2545c27af
Yossri Njeh <40792054+yossrinjeh@users.noreply.github.com>
2024-03-25 14:02:14 +0100

    init

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('frontOffice/base.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('frontOffice/about.html.twig');
+    }
+    #[Route('/contact', name: 'app_contact')]
+    public function contactUs(): Response
+    {
+        return $this->render('frontOffice/contact.html.twig');
+    }
+    #[Route('/backoffice', name: 'app_back')]
+    public function back(): Response
+    {
+        return $this->render('BackOffice/base.html.twig');
+    }
+}
