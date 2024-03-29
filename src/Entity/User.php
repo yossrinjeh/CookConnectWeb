60db95cdca4788767a7640dd6853bb42de854c2c
Yossri Njeh <40792054+yossrinjeh@users.noreply.github.com>
2024-03-29 14:29:29 +0100

    auth system

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * User
@@ -11,7 +13,8 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="user")
  * @ORM\Entity
  */
-class User
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface
 {
     /**
      * @var int
@@ -41,21 +44,21 @@ class User
      *
      * @ORM\Column(name="image", type="string", length=300, nullable=false)
      */
-    private $image;
+    private $image="test";
 
     /**
      * @var string
      *
      * @ORM\Column(name="role", type="string", length=300, nullable=false)
      */
-    private $role;
+    private $role="test";
 
     /**
      * @var string
      *
      * @ORM\Column(name="verification_code", type="string", length=300, nullable=false)
      */
-    private $verificationCode;
+    private $verificationCode="1234";
 
     /**
      * @var bool|null
@@ -67,9 +70,9 @@ class User
     /**
      * @var \DateTime
      *
-     * @ORM\Column(name="date", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
+     * @ORM\Column(name="date", type="datetime", nullable=true, options={"default"="CURRENT_TIMESTAMP"})
      */
-    private $date = 'CURRENT_TIMESTAMP';
+    private $date ;
 
     public function getId(): ?int
     {
@@ -160,5 +163,29 @@ class User
         return $this;
     }
 
+    public function getRoles(): array
+    {
+        return [$this->role];
+    }
 
+    public function getSalt(): ?string
+    {
+        // You can leave this method blank or return a salt
+        return null;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->email;
+    }
+
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+    public function getIdentifier():string
+    {
+        return $this->email;
+    }
 }
