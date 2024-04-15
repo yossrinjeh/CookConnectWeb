c62273deb826155c913d5810431517ff382a4ebf
Larguech-Med <159634932+Larguech-Med@users.noreply.github.com>
2024-04-15 22:53:44 +0100

    php

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Config\Security\FirewallConfig\FormLoginConfig;
 
 class IngredientController extends AbstractController{
 
@@ -15,11 +16,15 @@ class IngredientController extends AbstractController{
     public function new(Request $request):Response
     {
         $ingredient = new Ingredient();
-        $form = $this->createForm(IngredientType::class);
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form-> handleRequest($request);
+        //if($form->isSubmitted() && $form->isValid()){
+        //}
         return $this-> render('Ingredient/new.html.twig',[
             "form" => $form->createView()
         ]);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($ingredient);
+        $em->flush();
     }
-
-
 }
