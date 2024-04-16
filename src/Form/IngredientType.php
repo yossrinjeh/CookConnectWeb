51957f0dc395d2d8d08ad953b42412e8b6965fd3
Larguech-Med <159634932+Larguech-Med@users.noreply.github.com>
2024-04-16 23:14:29 +0100

    ingredient control saisie 1/2

--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -1,30 +1,56 @@
 <?php
 
 namespace App\Form;
-
 use App\Entity\Ingredient;
+use App\Entity\Nutrition;
+use App\Repository\NutritionRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class IngredientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('userId')
-            ->add('nom',TextType::class)
-            ->add('qte',IntegerType::class)
-            ->add('prixVente',NumberType::class)
-            ->add('prixAchat',NumberType::class)
-            ->add('idNutrition',IntegerType::class)
-            ->add('etat',)
-            ->add('quantiteThreshold',IntegerType::class)
-            ->add('image')
-        ;
+            // Remove 'userId' field as it's passed automatically
+            // ->add('userId')
+
+            ->add('nom', TextType::class, [
+                'label' => 'Name',
+            ])
+            ->add('qte', IntegerType::class, [
+                'label' => 'Quantity',
+            ])
+            ->add('prixVente', NumberType::class, [
+                'label' => 'Selling Price',
+            ])
+            ->add('prixAchat', NumberType::class, [
+                'label' => 'Purchase Price',
+            ])
+            ->add('idNutrition', IntegerType::class, [
+                'label' => 'Nutrition ID',
+                'constraints' => [
+                    new Callback([$this, 'NutritionIdExists'])
+                ]
+            ])
+            ->add('etat', TextType::class, [
+                'label' => 'State',
+            ])
+            ->add('quantiteThreshold', IntegerType::class, [
+                'label' => 'Quantity Threshold',
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Image',
+                'mapped' => false,
+                'required' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -32,5 +58,21 @@ class IngredientType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Ingredient::class,
         ]);
+    }
+
+    public function NutritionIdExists($value, ExecutionContextInterface $context): void
+    {
+        $nutritionId = $value;
+        $entityManager = $context->getObject()->getEntityManager();
+
+        // Check if the nutrition ID exists in the database
+        $nutrition = $entityManager->getRepository(Nutrition::class)->find($nutritionId);
+
+        if (!$nutrition) {
+            $context->buildViolation('The nutrition {{ id }} does not exist in the database.')
+                ->setParameter('{{ id }}',$nutritionId)
+                ->atPath('idNutrition')
+                ->addViolation();
+        }
     }
 }
