69d6cad19c96a3b0ff77b04fdf55b749c9682f90
Larguech-Med <159634932+Larguech-Med@users.noreply.github.com>
2024-04-22 15:37:04 +0100

    nutrition exist part recette

--- src/Form/RecetteNutritionType.php
+++ src/Form/RecetteNutritionType.php
@@ -4,14 +4,28 @@ namespace App\Form;
 
 use App\Entity\Recette;
 use App\Entity\Ingredient; 
+use App\Entity\Nutrition; 
+use App\Repository\NutritionRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class RecetteNutritionType extends AbstractType
 {
+
+    private $nutritionRepository;
+
+    public function __construct(NutritionRepository $nutritionRepository)
+    {
+        $this->nutritionRepository= $nutritionRepository;
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -21,8 +35,13 @@ class RecetteNutritionType extends AbstractType
                 'multiple' => true, 
                 'expanded' => true, 
             ])
-            ->add('quantiteIngredients',IntegerType::class)
-            ->add('idNutrition',IntegerType::class)
+            ->add('idIngredients',  TextType::class)
+            ->add('quantiteIngredients',TextType::class)
+            ->add('idNutrition',IntegerType::class,[
+                'constraints' =>[
+                    new Callback([$this, 'nutritionExists'])
+                ]
+            ])
         ;
     }
 
@@ -31,5 +50,23 @@ class RecetteNutritionType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Recette::class,
         ]);
+    }
+
+    public function nutritionExists($data ,ExecutionContextInterface $context) : void
+    {
+        $form = $context->getRoot();
+        /** @var App\Entity\Nutrition $formData */
+        $formData = $form->getData();
+
+        $nutritionId = $formData->getIdNutrition();
+
+        $nutrition = $this->nutritionRepository->find($nutritionId);
+        if(!$nutrition){
+            $context->buildViolation('Nutrition with ID {{ id }} does not exist.')
+                ->setParameter('{{ id }}', (string)$nutritionId)
+                ->atPath('idNutrition')
+                ->addViolation();
+        }
+
     }
 }
