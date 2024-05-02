c91df671ad939c5aecb5b9636bf5ad56605fdf93
Larguech-Med <159634932+Larguech-Med@users.noreply.github.com>
2024-05-02 09:17:56 +0100

    Update IngredientController.php

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -69,7 +69,6 @@ class IngredientController extends AbstractController
             }
 
             
-            $ingredient->setUserId(100);
             if($ingredient->getQte()<$ingredient->getQuantiteThreshold()){
                 $ingredient->setEtat("disabled");
             }else{
