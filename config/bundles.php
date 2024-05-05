a8c810b905d4ca00e8f2008173413820bf922216
Yossri Njeh <40792054+yossrinjeh@users.noreply.github.com>
2024-05-05 15:50:16 +0100

    fix WebpackEncoreBundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,7 +14,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     MeteoConcept\HCaptchaBundle\MeteoConceptHCaptchaBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
