b2fb19c0acbbb665f7664aa101b6200f540f649b
Iheb-Ferchichi <mohamediheb.ferchichi@esprit.tn>
2024-05-07 10:50:08 +0200

    update

--- migrations/Version20240506190303.php
+++ migrations/Version20240506190303.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240503133343 extends AbstractMigration
+final class Version20240506190303 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,10 @@ final class Version20240503133343 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commande ADD repas_id INT DEFAULT NULL, DROP id_repas');
+        $this->addSql('ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67D1D236AAA FOREIGN KEY (repas_id) REFERENCES repas (id)');
+        $this->addSql('CREATE INDEX IDX_6EEAA67D1D236AAA ON commande (repas_id)');
+        $this->addSql('ALTER TABLE livraison CHANGE id_commande id_commande INT DEFAULT NULL');
         $this->addSql('ALTER TABLE livraison ADD CONSTRAINT FK_A60C9F1F3E314AE8 FOREIGN KEY (id_commande) REFERENCES commande (id)');
         $this->addSql('CREATE INDEX IDX_A60C9F1F3E314AE8 ON livraison (id_commande)');
     }
@@ -27,7 +31,11 @@ final class Version20240503133343 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commande DROP FOREIGN KEY FK_6EEAA67D1D236AAA');
+        $this->addSql('DROP INDEX IDX_6EEAA67D1D236AAA ON commande');
+        $this->addSql('ALTER TABLE commande ADD id_repas VARCHAR(300) NOT NULL, DROP repas_id');
         $this->addSql('ALTER TABLE livraison DROP FOREIGN KEY FK_A60C9F1F3E314AE8');
         $this->addSql('DROP INDEX IDX_A60C9F1F3E314AE8 ON livraison');
+        $this->addSql('ALTER TABLE livraison CHANGE id_commande id_commande INT NOT NULL');
     }
 }
