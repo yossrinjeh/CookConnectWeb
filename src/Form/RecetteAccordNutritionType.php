fd5d7b749979c0f84f603738d1bbd8880d80f529
Larguech-Med <159634932+Larguech-Med@users.noreply.github.com>
2024-05-08 14:48:04 +0100

    fix backofficee bug

--- src/Form/RecetteAccordNutritionType.php
+++ src/Form/RecetteAccordNutritionType.php
@@ -3,24 +3,37 @@
 namespace App\Form;
 
 use App\Entity\Recette;
+use App\Repository\NutritionRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RecetteAccordNutritionType extends AbstractType
 {
+
+
+    private $nutritionRpository;
+
+    public function __construct(NutritionRepository $nutritionRepository)
+    {
+        $this->nutritionRpository = $nutritionRepository;
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('idUser')
-            ->add('nom')
-            ->add('prix')
-            ->add('description')
-            ->add('idIngredients')
-            ->add('quantiteIngredients')
-            ->add('idNutrition')
-            ->add('etat')
-            ->add('image')
+            ->add('idNutrition', IntegerType::class,[
+                'label' => 'Nutrition ID', 
+                'constraints' => [
+                    new NotBlank(['message'=>'this field is required',]),
+                    new Callback([$this, 'nutritionExist']),
+                ],
+            ])
         ;
     }
 
@@ -29,5 +42,26 @@ class RecetteAccordNutritionType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Recette::class,
         ]);
+    }
+
+    public function nutritionExist($data, ExecutionContextInterface $context): void
+    {
+        $formData = $context->getRoot()->getData();
+        $nutritionId = $formData->getIdNutrition();
+
+        $nutrition = $this->nutritionRpository->find($nutritionId);
+
+        if($nutrition){
+            if(!$nutrition){
+                $context->buildViolation('Nutrition with ID {{ id }} does not exist.')
+                    ->setParameter('{{ id }}', (string)$nutritionId)
+                    ->atPath('idIngredient')
+                    ->addViolation();   
+            }
+        }else{
+            $context->buildViolation('This field is required.')
+                    ->atPath('idIngredient')
+                    ->addViolation();
+        }
     }
 }
